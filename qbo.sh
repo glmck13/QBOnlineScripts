0ece2edc8d46f9b176a53ac2fd551f578ccc3b49
glmck13 <glmck13@verizon.net>
2015-11-20 11:54:10 -0500

    Create qbo.sh

---
+++ qbo.sh
@@ -0,0 +1,43 @@
+#!/bin/ksh
+
+METHOD=${1:?Enter METHOD}
+API=${2:?Enter API URI}
+shift; shift
+case "$METHOD" in
+	GET|POST)
+		;;
+	*)
+		print "METHOD not recognized: $METHOD"; exit
+		;;
+esac
+
+PARAMS=""
+while [ "$*" ]; do
+	PARAMS+="${1%%=*}=$(urlencode "${1#*=}")"; shift
+done
+
+TOKENFILE=$HOME/etc/${QBO_SANDBOX}qboTokens.conf
+
+. $TOKENFILE
+
+URL=$(eval print ${QBO_APIBASE}${API})
+
+OAUTH="$OAUTH_CONSUMER_KEY&$OAUTH_NONCE&$OAUTH_SIGNATURE_METHOD&$OAUTH_TIMESTAMP&$OAUTH_TOKEN&$OAUTH_VERSION"
+[ "$PARAMS" ] && OAUTH+="&$PARAMS"
+
+BASE="$METHOD&$(urlencode "$URL")&$(urlencode "$OAUTH")"
+SIGNATURE=$(echo -n "$BASE" | openssl dgst -sha1 -binary -hmac "${OAUTH_CONSUMER_SECRET#*=}&${OAUTH_TOKEN_SECRET#*=}" | base64 | xargs urlencode)
+OAUTH+="&oauth_signature=$SIGNATURE"
+
+case "$METHOD" in
+
+	GET)
+		curl -s -H "Accept: application/json" "$URL?$OAUTH"
+		;;
+
+	POST)
+		curl -s -H "Accept: application/json" -H "Content-Type: application/json" -d @- "$URL?$OAUTH"
+		;;
+esac
+
+print
