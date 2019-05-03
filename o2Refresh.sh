ca0194ec9b47e4722b4313298cfc1155ea4a02cd
glmck13 <glmck13@verizon.net>
2019-05-02 21:20:38 -0400

    Add files via upload

--- o2Refresh.sh
+++ o2Refresh.sh
@@ -9,16 +9,22 @@ REDIR=$(urlencode "$OAUTH2_CALLBACK")
 
 json=$(curl -s -H "Authorization: Basic $AUTH" -d "grant_type=refresh_token&refresh_token=$OAUTH2_REFRESH_TOKEN" "$OAUTH2_TOKEN_SERVER")
 
-token=$(print "$json" | sed -e 's/.*"access_token":"\([^"]\+\)".*/\1/')
+refresh_token=$(print "$json" | sed -e 's/.*"refresh_token":"\([^"]\+\)".*/\1/')
+access_token=$(print "$json" | sed -e 's/.*"access_token":"\([^"]\+\)".*/\1/')
 
 ed $TOKENFILE <<EOF
+/OAUTH2_REFRESH_TOKEN/
+d
+a
+OAUTH2_REFRESH_TOKEN="$refresh_token"
+.
 /OAUTH2_ACCESS_TOKEN/
 d
 a
-OAUTH2_ACCESS_TOKEN="$token"
+OAUTH2_ACCESS_TOKEN="$access_token"
 .
 w
 q
 EOF
 
-print $json; tail -1 $TOKENFILE
+print $json; tail -2 $TOKENFILE
