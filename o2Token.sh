ddf64bd6e758cc05be8e36edc66d7ce5a8b545d0
glmck13 <glmck13@verizon.net>
2019-09-07 15:10:01 -0400

    Update o2Token.sh

--- o2Token.sh
+++ o2Token.sh
@@ -5,4 +5,4 @@ TOKENFILE=$HOME/etc/${QBO_SANDBOX}qboTokens.conf
 
 . $TOKENFILE
 
-print "$OAUTH2_CONNECT_SERVER?client_id=$OAUTH2_CLIENT&response_type=code&scope=$QBO_SCOPE&redirect_uri=$(urlencode "$OAUTH2_CALLBACK")&state=none"
+print "$OAUTH2_CONNECT_SERVER?client_id=$OAUTH2_CLIENT&response_type=code&scope=$QBO_SCOPE&redirect_uri=$(urlencode "$OAUTH2_CALLBACK")&state=$QBO_SANDBOX"
